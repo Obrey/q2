3f2c09d836a654d0bc3e2fb0ff8f494d37ff74ce
Obrey <gaeple@naver.com>
2024-01-07 19:10:55 +0900

    테스트

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "obrey.github.q2",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
